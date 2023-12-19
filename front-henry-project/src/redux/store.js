752ff08de372df24c3a898fdd4220dcf4e8c0477
Martin Lamacchia <martinjiro22@gmail.com>
2023-12-19 20:02:25 -0300

    Incluyendo Redux al Admin

--- front-henry-project/src/redux/store.js
+++ front-henry-project/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import LoginSlice from "./Slices/LoginSlice";
 import viewProjectsSlice from "./Slices/viewProjectsSlice";
+import UsersSlice from "./Slices/viewUserSlice";
 
 export const store = configureStore({
   reducer: {
     viewProjects: viewProjectsSlice,
     userLogin: LoginSlice,
+    viewUsers: UsersSlice,
   },
 });
