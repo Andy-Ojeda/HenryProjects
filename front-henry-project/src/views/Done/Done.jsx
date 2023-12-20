2e0ac29bae37239141788999e5a4d4cf54168d36
Andy Ojeda <tuar_a@hotmail.com>
2023-12-20 14:02:50 -0300

    Se agrega vista 'done'

--- front-henry-project/src/views/Done/Done.jsx
+++ front-henry-project/src/views/Done/Done.jsx
@@ -1,43 +1,38 @@
-import React from 'react'
-import style from './OnGoing.module.css';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import style from './Done.module.css';
+import Filters from '../../Components/Filters/Filters';
 
 import { fetchAllProjects } from '../../redux/Slices/viewProjectsSlice.js';
 import { useDispatch, useSelector } from "react-redux";
 import Card from '../../Components/Cards/Card';
 import Nav from '../../Components/Nav/Nav';
 import Menu from '../../Components/Menu/Menu.jsx';
-import Filters from '../../Components/Filters/Filters';
-
-export default function OnGoing() {
 
 
-    const dispatch = useDispatch();
+export default function Done() {
 
-    const AllProjects = useSelector((state) => state.viewProjects.projects);
-  
-    const [projects, setProjects] = useState(null);
-  
-    
-  
-    useEffect(()=>{
-        console.log("Estado GLOBAL...", AllProjects); 
-        setProjects(AllProjects);
-        console.log("Projects LOCAL: ", projects)
-    },[AllProjects])
-  
-    useEffect(()=>{
-      console.log("STATEEEEE...", AllProjects);
-        console.log("Dentro del handleButton, dispatchando...");
-        dispatch(fetchAllProjects());
-    },[])
+  const dispatch = useDispatch();
+
+  const AllProjects = useSelector((state) => state.viewProjects.projects);
+
+  const [projects, setProjects] = useState(null);
+
   
 
+  useEffect(()=>{
+      console.log("Estado GLOBAL...", AllProjects); 
+      setProjects(AllProjects);
+      console.log("Projects LOCAL: ", projects)
+  },[AllProjects])
 
-
+  useEffect(()=>{
+    console.log("STATEEEEE...", AllProjects);
+      console.log("Dentro del handleButton, dispatchando...");
+      dispatch(fetchAllProjects());
+  },[])
 
   return (
-    <div className={style.contALL}>
+        <div className={style.contALL}>
             <div className={style.contNav}>
                 <Nav />
             </div>
@@ -51,10 +46,9 @@ export default function OnGoing() {
                       <Filters />
                     </div>
                     
-                    
                     <div className={style.content}>
                         {
-                            projects && projects.map((pro, id) => pro.state==="onGoing" && <Card projects={pro} key={id}/> ) 
+                            projects && projects.map((pro, id) => pro.state==="done" && <Card projects={pro} key={id}/> ) 
                                 
                             
                         }
@@ -74,5 +68,6 @@ export default function OnGoing() {
 
         </div>
 
+    
   )
 }
