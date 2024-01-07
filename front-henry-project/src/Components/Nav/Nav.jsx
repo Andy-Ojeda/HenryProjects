7f29f31a9aa3004bc599137fd8fe772d6ca917a6
giselecuello <giselecuello@gmail.com>
2024-01-07 18:28:28 -0300

    corrigiendo login

--- front-henry-project/src/Components/Nav/Nav.jsx
+++ front-henry-project/src/Components/Nav/Nav.jsx
@@ -12,7 +12,7 @@ export default function Nav() {
             <img src={Logo} alt="" onClick={()=>navigate("/")} />
         </div>
         <div className={style.contTitle}>
-            <h2>🖤 Henry Experiens 🖤</h2>
+            <h2></h2>
         </div>
         <div className={style.contUser}>
             <h4>User</h4>
